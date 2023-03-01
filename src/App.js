30bcd9ff787f00a17b4e5e66f6c5e2c3de6cbbdb
WDawid <wojciech.dawid@wat.edu.pl>
2023-03-01 21:24:04 +0100

    added text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          BYE boy barely dressed boyy
+          BYE boy troy on feature oy
         </p>
         <a
           className="App-link"
